bde0f34677f99164e22ecd7832c0e3abeceb23ea
koushik <koushik.b.s407@gmail.com>
2025-10-03 16:11:25 +0530

    wrote code for tree problem

--- my_new_branch_testing_file.py
+++ my_new_branch_testing_file.py
@@ -1,1 +1,27 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
 
+def find_min_in_tree(root):
+    if root is None:
+        return None
+    current = root
+    while current.left:
+        current = current.left
+    return current.val
+
+# Example usage:
+# Constructing a simple BST
+#       5
+#      / \
+#     3   7
+#    /
+#   2
+root = TreeNode(5)
+root.left = TreeNode(3)
+root.right = TreeNode(7)
+root.left.left = TreeNode(2)
+
+print("Smallest element in the tree:", find_min_in_tree(root))
